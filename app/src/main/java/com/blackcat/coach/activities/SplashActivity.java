91fd9cc4fac3a3b232f43dbe4313081b4c36fdcd
HelloWorld <sunqimin09@163.com>
2016-04-12 12:25:11 +0800

    减少不用图片

--- app/src/main/java/com/blackcat/coach/activities/SplashActivity.java
+++ app/src/main/java/com/blackcat/coach/activities/SplashActivity.java
@@ -3,9 +3,11 @@ package com.blackcat.coach.activities;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.blackcat.coach.R;
@@ -27,6 +29,14 @@ public class SplashActivity extends BaseNoFragmentActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            // 透明状态栏
+            getWindow().addFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            // 透明导航栏
+            getWindow().addFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
         setContentView(R.layout.activity_splash);
         initViews();
         initData();
